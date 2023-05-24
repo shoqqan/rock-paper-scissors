4c04bcceb1f3ea1b5b41af3bdbf6aee829b654d3
shoqqan <tataev.shokan@gmail.com>
2023-05-25 02:42:21 +0600

    final

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
@@ -14,8 +13,8 @@ function random(min, max) {
     }
 }
 
-const randomFromRPS = () =>{
-    switch (random(1,3)) {
+const randomFromRPS = () => {
+    switch (random(1, 3)) {
         case 1:
             return 'rock'
         case 2:
@@ -32,7 +31,7 @@ const images = {
 }
 
 class RPS {
-    constructor(role, x, y, velX, velY, size,image) {
+    constructor(role, x, y, velX, velY, size, image) {
         this.role = role
         this.x = x;
         this.y = y;
@@ -43,50 +42,67 @@ class RPS {
     }
 
     draw() {
-        this.image.src=images[this.role]
+        const img = new Image()
+        img.src = "./assets/back.jpg"
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0,width,height)
+        }
+        this.image.src = images[this.role]
         ctx.beginPath();
         ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        ctx.drawImage(this.image, this.x, this.y,this.size,this.size)
-
+        ctx.drawImage(this.image, this.x, this.y, this.size, this.size)
         ctx.fill();
 
     }
 
-    update() {
+    isSame() {
+        let isSame = true
+        for (const object of objectsRPS) {
+            if (object.role !== this.role) {
+                isSame = false
+            }
+        }
+        return isSame
+    }
 
-        if ((this.x + this.size) >= width) {
-            this.velX = -(Math.abs(this.velX));
+    update() {
+        if (!this.isSame()) {
+            if ((this.x + this.size) >= width) {
+                this.velX = -(Math.abs(this.velX));
+            }
+            if ((this.x - this.size) <= 0) {
+                this.velX = Math.abs(this.velX);
+            }
+            if ((this.y + this.size) >= height) {
+                this.velY = -(Math.abs(this.velY));
+            }
+            if ((this.y - this.size) <= 0) {
+                this.velY = Math.abs(this.velY);
+            }
+            this.x += this.velX;
+            this.y += this.velY;
+        } else {
+            document.querySelector('h1').innerText = `${this.role.toUpperCase()} WIN!!! \n PRESS F5 to restart`
         }
-        if ((this.x - this.size) <= 0) {
-            this.velX = Math.abs(this.velX);
-        }
-        if ((this.y + this.size) >= height) {
-            this.velY = -(Math.abs(this.velY));
-        }
-        if ((this.y - this.size) <= 0) {
-            this.velY = Math.abs(this.velY);
-        }
-        this.x += this.velX;
-        this.y += this.velY;
+
     }
 
     collisionDetect() {
-        for (const ball of objectsRPS) {
-            if (!(this === ball)) {
-                const dx = this.x - ball.x;
-                const dy = this.y - ball.y;
+        for (const object of objectsRPS) {
+            if (!(this === object)) {
+                const dx = this.x - object.x;
+                const dy = this.y - object.y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
-                if (distance < this.size + ball.size) {
-                    if (this.role === 'scissors' && ball.role === 'rock') {
+                if (distance < this.size + object.size) {
+                    if (this.role === 'scissors' && object.role === 'rock') {
                         this.role = 'rock'
                         this.color = images[this.role]
                     }
-                    if (this.role === 'paper' && ball.role === 'scissors') {
+                    if (this.role === 'paper' && object.role === 'scissors') {
                         this.role = 'scissors'
                         this.color = images[this.role]
                     }
-                    if (this.role === 'rock' && ball.role === 'paper') {
+                    if (this.role === 'rock' && object.role === 'paper') {
                         this.role = 'paper'
                         this.color = images[this.role]
                     }
@@ -106,7 +122,7 @@ while (objectsRPS.length < 20) {
     const size = 30;
     const objectRPS = new RPS(
         randomFromRPS(),
-        random(0 + size,    width - size),
+        random(0 + size, width - size),
         random(0 + size, height - size),
         random(-1, 1),
         random(-1, 1),
@@ -118,19 +134,13 @@ while (objectsRPS.length < 20) {
 }
 
 function loop() {
+
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
     ctx.fillRect(0, 0, width, height);
     for (const objectRockPaperScissor of objectsRPS) {
-        let isSame = true
-        if (objectRockPaperScissor.role !== this.role) {
-            isSame = false
-
-        }
-        if (!isSame) {
-            objectRockPaperScissor.draw();
-            objectRockPaperScissor.update();
-            objectRockPaperScissor.collisionDetect();
-        }
+        objectRockPaperScissor.draw();
+        objectRockPaperScissor.update();
+        objectRockPaperScissor.collisionDetect();
     }
     requestAnimationFrame(loop);
 }
