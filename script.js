8cc35f4b3d7f0a3e3e9c8434b8767ba42b1e1def
shoqqan <tataev.shokan@gmail.com>
2023-05-25 01:59:26 +0600

    change on image

--- script.js
+++ script.js
@@ -14,31 +14,45 @@ function random(min, max) {
     }
 }
 
-
-const colors = {
-    1: '#F50000',
-    2: '#0029F5',
-    3: '#00F510'
+const randomFromRPS = () =>{
+    switch (random(1,3)) {
+        case 1:
+            return 'rock'
+        case 2:
+            return 'paper'
+        case 3:
+            return 'scissors'
+    }
 }
 
-class RPS{
-    constructor(role, x, y, velX, velY, size) {
+const images = {
+    'rock': './assets/rock.png',
+    'paper': './assets/paper.png',
+    'scissors': './assets/scissors.png'
+}
+
+class RPS {
+    constructor(role, x, y, velX, velY, size,image) {
         this.role = role
         this.x = x;
         this.y = y;
         this.velX = velX;
         this.velY = velY;
-        this.color = colors[role];
         this.size = size;
+        this.image = image
     }
 
     draw() {
+        this.image.src=images[this.role]
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.drawImage(this.image, this.x, this.y,this.size,this.size)
+
         ctx.fill();
+
     }
-    
+
     update() {
 
         if ((this.x + this.size) >= width) {
@@ -58,23 +72,23 @@ class RPS{
     }
 
     collisionDetect() {
-        for (const ball of balls) {
+        for (const ball of objectsRPS) {
             if (!(this === ball)) {
                 const dx = this.x - ball.x;
                 const dy = this.y - ball.y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
                 if (distance < this.size + ball.size) {
-                    if (this.role === 1 && ball.role === 2) {
-                        this.role = 2
-                        this.color = colors[this.role]
+                    if (this.role === 'scissors' && ball.role === 'rock') {
+                        this.role = 'rock'
+                        this.color = images[this.role]
                     }
-                    if (this.role === 3 && ball.role === 1) {
-                        this.role = 1
-                        this.color = colors[this.role]
+                    if (this.role === 'paper' && ball.role === 'scissors') {
+                        this.role = 'scissors'
+                        this.color = images[this.role]
                     }
-                    if (this.role === 2 && ball.role === 3) {
-                        this.role = 3
-                        this.color = colors[this.role]
+                    if (this.role === 'rock' && ball.role === 'paper') {
+                        this.role = 'paper'
+                        this.color = images[this.role]
                     }
                     this.velX = random(-1, 1);
                     this.velY = random(-1, 1);
@@ -85,35 +99,37 @@ class RPS{
 
 }
 
-const balls = [];
+const objectsRPS = [];
 
-while (balls.length < 20) {
-    const size = 10;
-    const ball = new RPS(
-        random(1, 3),
-        random(0 + size, width - size),
+while (objectsRPS.length < 20) {
+    const image = new Image(20, 20)
+    const size = 30;
+    const objectRPS = new RPS(
+        randomFromRPS(),
+        random(0 + size,    width - size),
         random(0 + size, height - size),
         random(-1, 1),
         random(-1, 1),
-        size
+        size,
+        image
     );
 
-    balls.push(ball);
+    objectsRPS.push(objectRPS);
 }
 
 function loop() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
     ctx.fillRect(0, 0, width, height);
-    for (const ball of balls) {
+    for (const objectRockPaperScissor of objectsRPS) {
         let isSame = true
-        if (ball.role !== this.role) {
+        if (objectRockPaperScissor.role !== this.role) {
             isSame = false
 
         }
         if (!isSame) {
-            ball.draw();
-            ball.update();
-            ball.collisionDetect();
+            objectRockPaperScissor.draw();
+            objectRockPaperScissor.update();
+            objectRockPaperScissor.collisionDetect();
         }
     }
     requestAnimationFrame(loop);
